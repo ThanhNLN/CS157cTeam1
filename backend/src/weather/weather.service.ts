d5aaa9231b34376355a3391f2c628655b0731866
Alex Bui <alexnbui@gmail.com>
2025-05-08 16:15:45 -0700

    fix weather syncing

--- backend/src/weather/weather.service.ts
+++ backend/src/weather/weather.service.ts
@@ -3,6 +3,8 @@ import { Cron } from '@nestjs/schedule';
 import { Neo4jService } from 'src/services/neo4j/neo4j.service';
 import { WeatherHttpService } from 'src/services/weather-http/weather-http.service';
 
+import { write, writeFileSync } from 'fs';
+
 @Injectable()
 export class WeatherService {
   constructor(
@@ -21,7 +23,7 @@ export class WeatherService {
   @Cron('* * * * *')
   async syncWeatherData() {
     const locations: { lat: number; long: number }[] = [];
-    for (let lon = 67; lon <= 125; lon++) {
+    for (let lon = -125; lon <= -67; lon++) {
       for (let lat = 25; lat <= 49; lat++) {
         locations.push({ lat: lat, long: lon });
       }
@@ -31,16 +33,43 @@ export class WeatherService {
     const delay = (ms: number) =>
       new Promise((resolve) => setTimeout(resolve, ms));
 
+    // TODO: set weatherCost here
     for (const chunk of chunks) {
       const weatherData = await this.weatherHttpService.getWeatherData(chunk);
+
+      if (weatherData.error) {
+        console.error('Error fetching weather data:', weatherData.error);
+        return;
+      }
+
+      const transformedWeatherData = weatherData.map((data) => {
+        const weather = {
+          latitude: data.latitude,
+          longitude: data.longitude,
+          weatherCode: data.hourly.weather_code[0],
+          cloudCover: data.hourly.cloud_cover_high[0],
+          temperature: data.hourly.temperature_180m[0],
+          windSpeed: data.hourly.wind_speed_180m[0],
+          weatherCost: 0,
+        };
+        weather.weatherCost = 0;
+        return weather;
+      });
 
       const query = `
         UNWIND $weatherData AS weather
         MATCH (n:NAVAID)
-        WHERE abs(n.latitude - weather.lat) <= 0.5 AND abs(n.longitude - weather.long) <= 0.5
-        SET n.weather = weather.data
+        WHERE abs(n.latitude - weather.latitude) <= 0.5 AND abs(n.longitude - weather.longitude) <= 0.5
+        SET n.weatherCost = weather.weatherCost
+        SET n.weatherCode = weather.weatherCode
+        SET n.cloudCover = weather.cloudCover
+        SET n.temperature = weather.temperature
+        SET n.windSpeed = weather.windSpeed
       `;
-      console.log(await this.neo4jService.write(query, { weatherData }));
+
+      await this.neo4jService.write(query, {
+        weatherData: transformedWeatherData,
+      });
       await delay(11000);
     }
   }
