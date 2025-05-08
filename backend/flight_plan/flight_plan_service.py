dc12f83cae6c5ec370d42b2d03a377ebb6f3a4e8
Alex Bui <alexnbui@gmail.com>
2025-05-08 00:58:01 -0700

    switch to nest

--- backend/flight_plan/flight_plan_service.py
+++
@@ -1,6 +0,0 @@
-from services.neo4j_service import Neo4jService
-from services.redis_service import RedisService
-
-class TrafficService:
-    neo4j_service = Neo4jService()
-    redis_service = RedisService()
--- backend/traffic/traffic_service.py
+++
@@ -1,7 +0,0 @@
-from services.neo4j_service import Neo4jService
-
-class TrafficService:
-    neo4j_service = Neo4jService()
-
-    def update():
-        pass
